303854850118cf0a153151e2525b39392171e8e2
Abubakar Yahya Hamza <hamsukydeveloper@gmail.com>
2024-02-28 12:39:38 +0100

    first commit

---
+++ lib/Features/FireFighter/firefighter_dashboard.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FirefighterDashboard extends StatefulWidget {
+  const FirefighterDashboard({Key? key}) : super(key: key);
+
+  @override
+  State<FirefighterDashboard> createState() => _FirefighterDashboardState();
+}
+
+class _FirefighterDashboardState extends State<FirefighterDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Firefighter Dashboard"),
+      ),
+    );
+  }
+}
